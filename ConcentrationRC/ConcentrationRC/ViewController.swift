c10e19393f14e09fd8f0c49328964190092f8653
Roeut Chak <roeut.chak@gmail.com>
2019-09-16 23:12:55 -0400

    Add files via upload
    add files by dragging into github

---
+++ ConcentrationRC/ConcentrationRC/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ConcentrationRC
+//
+//  Created by Apple on 9/10/19.
+//  Copyright © 2019 Roeut Chak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+}
+
